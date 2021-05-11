4a8c18d271d83d48ee802eda411a570f089b2cd4
Hebilicious <xsh4k3@gmail.com>
2021-05-11 20:43:08 +0100

    feat: Implement Wallet Generation

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Mnemonic to private key
 
-Small package that converts a seed phrase to a master private key.
+Small package that can convert a seed phrase to a master private key and a public address.
+Can be used to generate those as well.
 
 ## How to use
 
 ```bash
+#Generate SeedPhrase, Private Key and Wallet
+npx mnemonic-to-private-key
+
+# Convert SeedPhrase to private key and wallet
 npx mnemonic-to-private-key "garlic pupil february legend morning bright section stool action stadium course busy"
 ```
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import Mnemonic from "bitcore-mnemonic"
+import elliptic from "elliptic"
+import { pubToAddress } from "ethereumjs-util"
 
-const getPrivateKey = (phrase) => {
-  const key = new Mnemonic(phrase)
-    .toHDPrivateKey()
-    .derive("m/44'/60'/0'/0/0")
-    .privateKey.toBuffer()
-    .toString("hex")
-
-  console.log(`
-    Mnemonic phrase: ${phrase}
-    Private Key : ${key}
-    `)
-  return key
+function padTo32(msg) {
+  while (msg.length < 32) {
+    msg = Buffer.concat([new Buffer([0]), msg])
+  }
+  if (msg.length !== 32) {
+    throw new Error(`invalid key length: ${msg.length}`)
+  }
+  return msg
 }
 
-export default getPrivateKey
+export const createPrivateKey = (seedPhrase) => {
+  const mnemonic = new Mnemonic(seedPhrase)
+  const phrase = mnemonic.toString()
+
+  const derived = mnemonic.toHDPrivateKey().derive("m/44'/60'/0'/0/0")
+
+  const key = derived.privateKey.toBuffer().toString("hex")
+  const publicKey = derived.publicKey.toBuffer()
+  const ecPublic = new elliptic.ec("secp256k1").keyFromPublic(publicKey).getPublic().toJSON()
+  const ethPublic = Buffer.concat([
+    padTo32(Buffer.from(ecPublic[0].toArray())),
+    padTo32(Buffer.from(ecPublic[1].toArray()))
+  ])
+  const address = `0x${pubToAddress(ethPublic).toString("hex")}`
+
+  // console.dir(derived)
+  console.log(`
+    Mnemonic phrase: ${phrase}
+    Address: ${address}
+    Private Key : ${key}
+    `)
+  return { phrase, address, key }
+}
+export default createPrivateKey
